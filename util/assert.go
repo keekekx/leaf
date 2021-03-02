609c6c9bb5ce10df8b565e3fc223eae0d056ad9d
shuke <shuke.blackflag@gmail.com>
2021-03-02 17:31:51 +0800

    提供一个创建错误消息的接口

--- util/assert.go
+++ util/assert.go
@@ -12,6 +12,13 @@ func (e *ErrorInfo) Error() string {
 	return fmt.Sprintf("Code:%s,Debug:%s", e.Code, e.Debug)
 }
 
+func NewError(code interface{}, debug string) *ErrorInfo {
+	return &ErrorInfo{
+		Code : code,
+		Debug: debug,
+	}
+}
+
 func Assert(check bool, code interface{}, debug string) {
 	if !check {
 		panic(&ErrorInfo{
