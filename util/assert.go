28b25fbde4fdea99f1407bd3f9a43f28062ace9f
shuke <shuke.blackflag@gmail.com>
2021-03-03 12:27:20 +0800

    从框架移除断言逻辑到服务器

--- util/assert.go
+++ util/assert.go
@@ -1,6 +1,8 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ErrorInfo struct {
 	Code interface{}
@@ -10,38 +12,4 @@ type ErrorInfo struct {
 
 func (e *ErrorInfo) Error() string {
 	return fmt.Sprintf("Code:%s,Debug:%s", e.Code, e.Debug)
-}
-
-func NewError(code interface{}, debug string) *ErrorInfo {
-	return &ErrorInfo{
-		Code : code,
-		Debug: debug,
-	}
-}
-
-func Panic( code interface{}, debug string, kick bool){
-	panic(&ErrorInfo{
-		Code : code,
-		Debug: debug,
-		Kick: kick,
-	})
-}
-
-func Assert(check bool, code interface{}, debug string) {
-	if !check {
-		panic(&ErrorInfo{
-			Code : code,
-			Debug: debug,
-		})
-	}
-}
-
-func AssertKick(check bool, code interface{}, debug string) {
-	if !check {
-		panic(&ErrorInfo{
-			Code : code,
-			Debug: debug,
-			Kick: true,
-		})
-	}
 }
